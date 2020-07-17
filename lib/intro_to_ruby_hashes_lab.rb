b76006ff49973e78249d3087514fc6164f19af94
Waverley Leung <waverley.nyc@gmail.com>
2020-07-17 13:47:09 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,7 +1,7 @@
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
   hash = {}
-  hash[:key = value]
+  hash[:key] = value
 end
 
 def read_from_hash(hash, key)
